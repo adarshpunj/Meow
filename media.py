dba626dcb9bdf4a10398f640a60edf696b06212e
Adarsh Punj <30762976+adarshpunj@users.noreply.github.com>
2019-01-30 12:44:07 +0530

    Updated

---
+++ media.py
@@ -0,0 +1,21 @@
+import requests
+
+base_url = 'https://www.google.com/logos/fnbx/animal_sounds/'
+
+animal_names = ['african-grey-parrot','alligator','ape','bat','bee',
+'buffalo','camel','cat','rooster','cow','crow','dog','duck','elephant',
+'frog','giraffe','horse','leopard','lion','moose','owl',
+'penguin','pig','rat','rattlesnake','rhinoceros','sheep','tiger',
+'wolf','zebra']
+
+def download_file(extension):
+	for animal_name in animal_names:
+		try:
+			file = requests.get(base_url+animal_name+extension,allow_redirects=True)
+			open(animal_name+extension,'wb').write(file.content)
+			print "DOWNLOADED: "+animal_name+extension
+		except:
+			print "ERROR WHILE DOWNLOADING "+animal_name+extension
+
+download_file('.mp3')
+download_file('.png')
